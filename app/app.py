08203169f5fe9d597b170230220480f637352f2f
unknown <alexandar.kaukov@gmail.com>
2026-06-25 19:08:13 +0300

    add docker compose, redis and redis health check

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
 from datetime import datetime, timezone
 import os
-
+import redis
 
 app = Flask(__name__)
 
 SERVICE_NAME = os.getenv("SERVICE_NAME", "azure-cloud-devops-api")
 APP_VERSION = os.getenv("APP_VERSION", "1.0.0")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 
 
 @app.route("/")
@@ -48,6 +50,34 @@ def api_status():
             "cache": "not_configured"
         }
     })
+
+@app.route("/redis")
+def redis_check():
+    try:
+        client = redis.Redis(
+            host=REDIS_HOST,
+            port=REDIS_PORT,
+            decode_responses=True
+        )
+
+        client.set("health_check", "ok")
+        value = client.get("health_check")
+
+        return jsonify({
+            "redis_status": "connected",
+            "redis_host": REDIS_HOST,
+            "redis_port": REDIS_PORT,
+            "test_key": "health_check",
+            "test_value": value
+        })
+
+    except Exception as error:
+        return jsonify({
+            "redis_status": "error",
+            "redis_host": REDIS_HOST,
+            "redis_port": REDIS_PORT,
+            "error": str(error)
+        }), 500
 
 
 if __name__ == "__main__":
